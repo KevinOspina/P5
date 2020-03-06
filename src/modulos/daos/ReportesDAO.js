3c95acfdb63229e6b047dbe10122d0871c091a33
JuanELG <juane.leal98@gmail.com>
2020-03-06 11:05:12 -0500

    Datos de reportes terminados y consulta en el DAO terminada, funcionando

--- src/modulos/daos/ReportesDAO.js
+++ src/modulos/daos/ReportesDAO.js
@@ -11,8 +11,8 @@ class ReportesDAO extends DaoHandlerJS {
         });
     }
 
-    getReportesByCuadrilla(cuadrilla, area, callback){
-        this.exec_query("select r.Id_reporte as id, r.Tipo, r.Descripcion, ar.nombre  from reportes r, actividades a, areas ar where r.Id_eventos = a.Id_eventos and r.Id_areas = ",area," and Id_cuadrillas = ",cuadrilla," ", null, (err, row, fields) => {
+    getReportesByCuadrilla(Id_areas, Id_cuadrillas, callback){
+        this.exec_query('select distinct r.Id_reportes as id, r.Tipo, r.Descripcion, ar.nombre from reportes r, actividades a, areas ar, cuadrillas c where r.Id_eventos = a.Id_eventos and r.Id_areas = ar.Id_areas and a.Id_cuadrillas = c.Id_cuadrillas and r.Id_areas =? and a.Id_cuadrillas =?',[Id_areas,Id_cuadrillas], (err, row, fields) => {
             callback(err, row, fields);
         });
     }
